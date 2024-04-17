66290876660271fe648b0aa79c98fd278b47dc9a
Florent Bourgeois <florent@twic.pics>
2024-04-17 18:42:52 +0200

    Adds GetPicture function

---
+++ netlify/functions/getPicture.mjs
@@ -0,0 +1,7 @@
+import { getStore } from '@netlify/blobs'
+
+export default async (req, context) => {
+  const store = getStore('idisappear')
+  const blob = await store.get('picture', { type: 'blob' })
+  return new Response(blob)
+}
