8f096eceb3a09556c700c0f72142ef4f7e1fcbbf
Florent Bourgeois <florent@twic.pics>
2024-05-05 23:57:46 +0200

    Optimizes Edge function caching

--- netlify/edge-functions/counter.mjs
+++ netlify/edge-functions/counter.mjs
@@ -1,11 +1,34 @@
+import crypto from "crypto";
 import { getStore } from "@netlify/blobs";
 
-export default async () => {
+export default async (req) => {
   const store = getStore("idisappear");
   const counter = await store.get("counter", { type: "json" });
-  return new Response(counter);
+  const etag = `"${crypto.createHash("md5").update(counter).digest("hex")}"`;
+
+  const headers = {
+    "Content-Type": "	application/json",
+    "Cache-Control": "public, max-age=0, must-revalidate", // Tell browsers to always revalidate
+    "Netlify-CDN-Cache-Control": "public, max-age=31536000, must-revalidate", // Tell Edge to cache asset for up to a year
+    ETag: etag,
+  };
+
+  if (
+    req.headers.get("if-none-match") === `W/${etag}` ||
+    req.headers.get("if-none-match") === etag
+  ) {
+    return new Response(null, {
+      status: 304,
+      headers: { headers },
+    });
+  }
+
+  return new Response(counter, {
+    headers: headers,
+  });
 };
 
 export const config = {
+  cache: "manual",
   path: "/api/counter",
 };
--- netlify/edge-functions/picture.mjs
+++ netlify/edge-functions/picture.mjs
@@ -1,3 +1,4 @@
+import crypto from "crypto";
 import { getStore } from "@netlify/blobs";
 
 export default async (req, context) => {
@@ -8,9 +9,34 @@ export default async (req, context) => {
   } else {
     blob = await store.get("picture", { type: "blob" });
   }
-  return new Response(blob);
+
+  const blobString = await blob.text();
+
+  const etag = `"${crypto.createHash("md5").update(blobString).digest("hex")}"`;
+
+  const headers = {
+    "Content-Type": "	image/png",
+    "Cache-Control": "public, max-age=0, must-revalidate", // Tell browsers to always revalidate
+    "Netlify-CDN-Cache-Control": "public, max-age=31536000, must-revalidate", // Tell Edge to cache asset for up to a year
+    ETag: etag,
+  };
+
+  if (
+    req.headers.get("if-none-match") === `W/${etag}` ||
+    req.headers.get("if-none-match") === etag
+  ) {
+    return new Response(null, {
+      status: 304,
+      headers: { headers },
+    });
+  }
+
+  return new Response(blob, {
+    headers: headers,
+  });
 };
 
 export const config = {
+  cache: "manual",
   path: "/api/picture{/:id}?",
 };
